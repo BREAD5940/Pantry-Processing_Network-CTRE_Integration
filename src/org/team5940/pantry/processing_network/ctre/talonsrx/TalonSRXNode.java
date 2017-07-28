de122536d60bb040a207ee04447b21190c49622c
David Boles <david.e.boles22@gmail.com>
2017-07-28 16:53:52 -0700

    Created minimal node for controlling Talon SRXs.

---
+++ src/org/team5940/pantry/processing_network/ctre/talonsrx/TalonSRXNode.java
@@ -0,0 +1,53 @@
+package org.team5940.pantry.processing_network.ctre.talonsrx;
+
+import org.team5940.pantry.processing_network.Network;
+import org.team5940.pantry.processing_network.Node;
+import org.team5940.pantry.processing_network.ValueNode;
+
+import com.ctre.CANTalon;
+
+/**
+ * This class accepts input from various source nodes provided on initialization and uses their values (or defaults if null) to control one of more CANTalon objects.
+ * @author David Boles
+ *
+ */
+public class TalonSRXNode extends Node {
+
+	/**
+	 * Stores the array of talons to control.
+	 */
+	CANTalon[] talons;
+	
+	/**
+	 * Stores the ValueNode for .set()
+	 */
+	ValueNode<? extends Double> setValue;
+	
+	/**
+	 * Constructs a new {@link TalonSRXNode}. Calls {@link Node#Node(Network, boolean, ValueNode...)} with (network, true, provided value nodes).
+	 * @param network The network that this node will be a member of.
+	 * @param A source for the value to .set() the talons. Default is 0 if null.
+	 * @param talons One or more talons that this controls.
+	 * @throws IllegalArgumentException talons contains null or thrown by super constructor.
+	 * @throws IllegalStateException super constructor throws it.
+	 */
+	public TalonSRXNode(Network network, ValueNode<? extends Double> setValue, CANTalon... talons)
+			throws IllegalArgumentException, IllegalStateException {
+		super(network, true);//TODO add sources
+		
+		for(CANTalon talon : talons) {
+			if(talon == null) {
+				throw new IllegalArgumentException("Null Talon");
+			}
+		}
+		this.talons = talons;
+	}
+
+	@Override
+	protected void doUpdate() {
+		for(CANTalon talon : this.talons) {
+			talon.set((this.setValue == null) ? 0 : this.setValue.getValue());
+		}
+	}
+
+}
