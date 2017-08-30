f0b1119978b5b6a75c943936906c50497b381d51
BertTurtle <mbentley124@gmail.com>
2017-08-29 17:56:19 -0700

    Updated argument checking to use built in ProcessingNetworkUtils argument checking.
    Also formatted code.

--- src/org/team5940/pantry/processing_network/ctre/TalonSRXNode.java
+++ src/org/team5940/pantry/processing_network/ctre/TalonSRXNode.java
@@ -2,12 +2,16 @@ package org.team5940.pantry.processing_network.ctre;
 
 import org.team5940.pantry.processing_network.Network;
 import org.team5940.pantry.processing_network.Node;
+import org.team5940.pantry.processing_network.ProcessingNetworkUtils;
 import org.team5940.pantry.processing_network.ValueNode;
 
 import com.ctre.CANTalon;
 
 /**
- * This class accepts input from various source nodes provided on initialization and uses their values (or defaults if null) to control one of more CANTalon objects.
+ * This class accepts input from various source nodes provided on initialization
+ * and uses their values (or defaults if null) to control one of more CANTalon
+ * objects.
+ * 
  * @author David Boles
  *
  */
@@ -17,36 +21,42 @@ public class TalonSRXNode extends Node {
 	 * Stores the array of talons to control.
 	 */
 	CANTalon[] talons;
-	
+
 	/**
 	 * Stores the ValueNode for .set()
 	 */
 	ValueNode<? extends Number> setValue;
-	
+
 	/**
-	 * Constructs a new {@link TalonSRXNode}. Calls {@link Node#Node(Network, boolean, ValueNode...)} with (network, true, provided value nodes).
-	 * @param network The network that this node will be a member of.
-	 * @param A source for the value to .set() the talons. Default is 0 if null.
-	 * @param talons One or more talons that this controls.
-	 * @throws IllegalArgumentException talons contains null or thrown by super constructor.
-	 * @throws IllegalStateException super constructor throws it.
+	 * Constructs a new {@link TalonSRXNode}. Calls
+	 * {@link Node#Node(Network, boolean, ValueNode...)} with (network, true,
+	 * provided value nodes).
+	 * 
+	 * @param network
+	 *            The network that this node will be a member of.
+	 * @param A
+	 *            source for the value to .set() the talons. Default is 0 if
+	 *            null.
+	 * @param talons
+	 *            One or more talons that this controls.
+	 * @throws IllegalArgumentException
+	 *             talons contains null or thrown by super constructor.
+	 * @throws IllegalStateException
+	 *             super constructor throws it.
 	 */
-	public TalonSRXNode(Network network, ValueNode<? extends Number> setValue, CANTalon... talons)
-			throws IllegalArgumentException, IllegalStateException {
-		super(network, true, setValue);
-		
-		for(CANTalon talon : talons) {
-			if(talon == null) {
-				throw new IllegalArgumentException("Null Talon");
-			}
-		}
+	public TalonSRXNode(Network network, boolean requireUpdate, ValueNode<? extends Number> setValue,
+			CANTalon... talons) throws IllegalArgumentException, IllegalStateException {
+		super(network, requireUpdate, setValue);
+
+		ProcessingNetworkUtils.checkArrayArguments(talons);
+
 		this.talons = talons;
 	}
 
 	@Override
 	protected void doUpdate() {
-		for(CANTalon talon : this.talons) {
-			talon.set((this.setValue == null) ? 0 : this.setValue.getValue().doubleValue());
+		for (CANTalon talon : this.talons) {
+			talon.set(this.setValue.getValue().doubleValue());
 		}
 	}
 
