0929e40427648ea3716124f56b43b2d4ae34b3c1
BertTurtle <mbentley124@gmail.com>
2017-08-30 11:38:16 -0700

    Created TalonSRXEncoderVelocityValueNode

---
+++ src/org/team5940/pantry/processing_network/ctre/output/TalonSRXEncoderVelocityValueNode.java
@@ -0,0 +1,36 @@
+package org.team5940.pantry.processing_network.ctre.output;
+
+import org.team5940.pantry.processing_network.Network;
+import org.team5940.pantry.processing_network.ProcessingNetworkUtils;
+import org.team5940.pantry.processing_network.ValueNode;
+
+import com.ctre.CANTalon;
+
+public class TalonSRXEncoderVelocityValueNode extends ValueNode<Integer> {
+
+	CANTalon encoderTalon;
+
+	/**
+	 * Stores the velocity of the encoder for the talon. This uses
+	 * {@link CANTalon#getEncVelocity()} to find the velocity. Does not do
+	 * anything to change it after that.
+	 * 
+	 * @param network
+	 * @param encoderTalon
+	 * @throws IllegalArgumentException
+	 * @throws IllegalStateException
+	 */
+	public TalonSRXEncoderVelocityValueNode(Network network, CANTalon encoderTalon)
+			throws IllegalArgumentException, IllegalStateException {
+		super(network);
+		// TODO Auto-generated constructor stub
+		ProcessingNetworkUtils.checkArgument(encoderTalon);
+		this.encoderTalon = encoderTalon;
+	}
+
+	@Override
+	protected Integer updateValue() {
+		return this.encoderTalon.getEncVelocity();
+	}
+
+}
